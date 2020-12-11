30e76aed3548b5dd63081cbeed855746466bd54c
Joseph Giunta <jgnohack@gmail.com>
2020-12-11 15:32:12 -0800

    add functionality to allow user to re-enter input if entered incorrectly

--- Bakery/program.cs
+++ Bakery/program.cs
@@ -21,28 +21,47 @@ public class Program
     BakeryBread breadOrder = new BakeryBread();
     BakeryPastry pastryOrder = new BakeryPastry();
     ShoppingCart order = new ShoppingCart(0, 0, 0);
+    bool validInput = false;
 
 		Console.WriteLine("Welcome to Pierre's Bakery! We have 2 special deals for you today: Bread is buy 2, get 1 free, and pastries are 3 for $5!");
     Console.WriteLine("Our regular prices are 1 loaf of bread is $5, and pastries are $2 each.");
     Console.WriteLine("Now, what can I get for you? Make sure you include any free loaves in your order!");
-    try
+    
+    while(!validInput)
     {
       Console.WriteLine("Bread:");
-      order.breadCount = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("Pastries:");
-      order.pastryCount = Convert.ToInt32(Console.ReadLine());
-      order.orderPrice = breadOrder.GetBreadPrice(order.breadCount) + pastryOrder.GetPastryPrice(order.pastryCount);
-
-      Console.WriteLine($"Your total price for {order.breadCount} loaves of bread, and {order.pastryCount} pastries is:");
-      Console.WriteLine($"${order.orderPrice}");
-    } 
-    catch(Exception err)
-    {
+      if(int.TryParse(Console.ReadLine(), out int userInput))
+      {
+        order.breadCount = userInput;
+        validInput = true;
+      }
+      else
+      {
       Console.ForegroundColor = ConsoleColor.Red;
-      Console.WriteLine($"Error caught: {err}{Environment.NewLine}");
-      Console.ForegroundColor = ConsoleColor.Green;
       Console.WriteLine("Make sure you only enter integers");
       Console.ResetColor();
+      }
     }
-	}
+
+    validInput = false;
+    while(!validInput)
+    {
+      Console.WriteLine("Pastries:");
+      if(int.TryParse(Console.ReadLine(), out int userInput))
+      {
+        order.pastryCount = userInput;
+        validInput = true;
+      }
+      else
+      {
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine("Make sure you only enter integers");
+      Console.ResetColor();
+      }
+    }
+    order.orderPrice = breadOrder.GetBreadPrice(order.breadCount) + pastryOrder.GetPastryPrice(order.pastryCount);
+
+    Console.WriteLine($"Your total price for {order.breadCount} loaves of bread, and {order.pastryCount} pastries is:");
+    Console.WriteLine($"${order.orderPrice}");
+  }
 }
