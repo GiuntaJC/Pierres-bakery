11bea179d94dad01e76b965b09fe97de2efe0994
Joseph Giunta <jgnohack@gmail.com>
2020-12-11 14:27:17 -0800

    add shoppingcart class, constructor and properties

--- Bakery/program.cs
+++ Bakery/program.cs
@@ -1,5 +1,18 @@
 using System;
 
+public class ShoppingCart
+{
+  public int orderPrice {get; set;}
+  public int pastryCount {get; set;}
+  public int breadCount {get; set;}
+
+  public ShoppingCart(int price, int pastries, int bread)
+  {
+    orderPrice = price;
+    pastryCount = pastries;
+    breadCount = bread;
+  }
+}
 public class Program
 {
 	public static void Main()
