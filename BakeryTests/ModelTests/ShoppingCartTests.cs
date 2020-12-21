ac84d4eabc73e3c8d80544eee5c5b3852c590479
Joseph Giunta <jgnohack@gmail.com>
2020-12-20 16:14:40 -0800

    add and fix rest of shopping cart tests

--- BakeryTests/ModelTests/ShoppingCartTests.cs
+++ BakeryTests/ModelTests/ShoppingCartTests.cs
@@ -3,16 +3,37 @@ using Bakery.Models;
 
 namespace Bakery.TestTools
 {
+  [TestClass]
   public class BakeryShoppingCartTests
   {
     [TestMethod]
-    public void AddOrder_AssociatesOrderWithVendor_OrderList()
+    public void ShoppingCart_AutoImplementedPropertiesCorrectlyAssign_OrderPrice()
     {
       ShoppingCart newOrder = new ShoppingCart(3, 4, 5);
 
       int result = 3;
 
       Assert.AreEqual(newOrder.OrderPrice, result);
+    }
+
+    [TestMethod]
+    public void ShoppingCart_AutoImplementedPropertiesCorrectlyAssign_PastryCount()
+    {
+      ShoppingCart newOrder = new ShoppingCart(3, 4, 5);
+
+      int result = 4;
+
+      Assert.AreEqual(newOrder.PastryCount, result);
+    }
+
+    [TestMethod]
+    public void ShoppingCart_AutoImplementedPropertiesCorrectlyAssign_BreadCount()
+    {
+      ShoppingCart newOrder = new ShoppingCart(3, 4, 5);
+
+      int result = 5;
+
+      Assert.AreEqual(newOrder.BreadCount, result);
     }
   }
 }
